547fdeee882f71f80a8afccce8f883e26c87f763
Khánh Pluto <tranvankhanh99a@gmail.com>
2024-06-09 00:32:17 +0700

    feat(product): create product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Clothing Shop';
 }
